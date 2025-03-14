f9a0d5f4d2773bb91f1dcc7ba65a0b963a135044
vahermihkel <65609256+vahermihkel@users.noreply.github.com>
2025-03-14 12:35:05 +0200

    pkt

---
+++ 04_28.02_proovikontrolltoo1/libisev-keskmine/src/test/java/ee/mihkel/libisev_keskmine/LibisevKeskmineApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.mihkel.libisev_keskmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibisevKeskmineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
